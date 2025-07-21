bfa06dc65fbf9dac7b0f00b52cc0711c3bebfd6b
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-07-21 10:50:03 +0530

    Pattern 5 added

--- src/Patterns/PatternsExercise.java
+++ src/Patterns/PatternsExercise.java
@@ -4,7 +4,7 @@ public class PatternsExercise {
 
     public static void main(String[] args) {
         PatternsExercise p = new PatternsExercise();
-        p.pattern3();
+        p.pattern5();
     }
 
     public void pattern1() {
@@ -38,6 +38,22 @@ public class PatternsExercise {
         for(int i = 1; i<=5; i++) {
             for(int j = 1; j<=i; j++) {
                 System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public void pattern5() {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("⭐");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("⭐");
             }
             System.out.println();
         }
