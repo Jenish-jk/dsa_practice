93433f09b42e9b4541c5200aefb2023fee927ad0
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-07-21 11:49:06 +0530

    Leetcode changes

---
+++ src/LeetCode/DeleteCharactersInString.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class DeleteCharactersInString {
+    public static void main(String[] args) {
+        String s = "Leeetcode";
+        System.out.println(makeFancyString(s));
+    }
+
+    public static String makeFancyString(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+
+        result.append(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count <= 2) {
+                result.append(s.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+}
