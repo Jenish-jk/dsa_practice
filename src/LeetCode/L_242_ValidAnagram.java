bc5ad1a0b289864f9f1866a42af426d8e2eb4f00
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-07-30 11:21:28 +0530

    Today's Leetcode Practice

---
+++ src/LeetCode/L_242_ValidAnagram.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class L_242_ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static Boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+
+    }
+}
