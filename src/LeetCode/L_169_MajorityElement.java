20a8bc2e929585e46fab9561183f756096cec79a
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-08-04 15:05:56 +0530

    Today's Leetcode Practice

---
+++ src/LeetCode/L_169_MajorityElement.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class L_169_MajorityElement {
+    public static void main(String[] args) {
+        int [] arr = {3, 2, 3};
+        System.out.println(majorityElement(arr));
+    }
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
---
+++ src/LeetCode/L_75_SortColors.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class L_75_SortColors {
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 2, 1, 1, 0};
+        sortColors(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sortColors(int [] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--){
+                if(nums[j] < nums[j -1]){
+                    swap(nums, j, j-1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
