e0f806d2ebdb77cfefe57526c43814ab1beac34a
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-09-25 14:56:11 +0530

    Today's Leetcode Practice

--- src/LeetCode/L_349_IntersectionOfTwoArrays.java
+++ src/LeetCode/L_349_IntersectionOfTwoArrays.java
@@ -1,4 +1,35 @@
 package LeetCode;
 
+import java.util.*;
+
 public class L_349_IntersectionOfTwoArrays {
+
+    public static void main(String[] args) {
+        int [] nums1 = {1, 9, 2, 1};
+        int [] nums2 = {9, 4, 9, 8, 4};
+        System.out.println(Arrays.toString(intersection(nums1, nums2)));
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int nums : nums1) {
+            set1.add(nums);
+        }
+
+        Set<Integer> resultSet = new HashSet<>();
+        for (int nums : nums2) {
+            if (set1.contains(nums)) {
+                resultSet.add(nums);
+            }
+        }
+
+        int [] result = new int [resultSet.size()];
+        int i = 0;
+
+        for (int nums : resultSet) {
+            result[i++] = nums;
+        }
+
+        return result;
+    }
 }
--- src/LeetCode/L_4_MedianOfTwoSortedArrays.java
+++ src/LeetCode/L_4_MedianOfTwoSortedArrays.java
@@ -1,4 +1,59 @@
 package LeetCode;
 
+
 public class L_4_MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int [] nums1 = {1, 3};
+        int [] nums2 = {2};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int imin = 0, imax = m;
+        int halfLen = (m + n + 1) / 2;
+
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                imin = i + 1;
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                imax = i - 1;
+            } else {
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                }else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+
+        throw new IllegalArgumentException("Input arrays are not sorted!");
+    }
 }
