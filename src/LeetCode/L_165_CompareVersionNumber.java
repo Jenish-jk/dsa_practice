481003202b5f8b7213d3544e8d5a54d398bbdb98
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-11-11 09:29:44 +0530

    Leetcode

---
+++ src/LeetCode/L_165_CompareVersionNumber.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class L_165_CompareVersionNumber {
+    public static void main(String[] args) {
+        String version1 = "1.2";
+        String version2 = "1.001";
+        System.out.println(compareVersion(version1, version2 ));
+    }
+
+    public static int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int length = Math.max(v1.length, v2.length);
+
+        for (int i = 0; i < length; i++) {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+        }
+
+        return 0;
+    }
+}
