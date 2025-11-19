168030f4373e5f4b286484d803544552c6845eab
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-11-19 10:47:19 +0530

    Leetcode practice

---
+++ src/LeetCode/L_29_DivideTwoIntegers.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+public class L_29_DivideTwoIntegers {
+    public static void main(String[] args) {
+        int dividend = 7;
+        int divisor = -3;
+        System.out.println(divide(dividend, divisor));
+    }
+
+    public static int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        int result = 0;
+
+        while (a >= b) {
+            long temp = b;
+            int count = 1;
+
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                count <<= 1;
+            }
+
+            a -= temp;
+            result += count;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0)) {
+            result = -result;
+        }
+
+        return result;
+    }
+}
+
