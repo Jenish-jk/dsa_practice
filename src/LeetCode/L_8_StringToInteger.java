f44629c83e0a09c927a171a7c2837f786b38d269
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-12-09 09:57:50 +0530

    Today's Leetcode Practice

---
+++ src/LeetCode/L_8_StringToInteger.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+public class L_8_StringToInteger {
+
+    public static void main(String[] args) {
+        String s = "42";
+        System.out.println(myAtoi(s));
+    }
+
+    public static int myAtoi(String s) {
+        int i = 0, n = s.length();
+        long result = 0;
+        int sign = 1;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int) result * sign;
+    }
+}
