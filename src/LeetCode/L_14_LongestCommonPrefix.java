a0e1874db98cb8290ce4d72690e9350942902a56
jenish-at-147099166213 <jenish.j@althisolutions.com>
2025-12-10 09:31:38 +0530

    Today's Leetcode Practice

---
+++ src/LeetCode/L_14_LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+public class L_14_LongestCommonPrefix {
+    public static void main(String[] args) {
+        String [] str = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(str));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+}
